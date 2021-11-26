9102b81096bddc41d261e2f800387fbe493fe0a5
Ioannis Nicolis <ioannis.nicolis@u-paris.fr>
2021-11-26 20:42:23 +0100

    Small typo correction

--- Day26/vaccination_gov_gr.py
+++ Day26/vaccination_gov_gr.py
@@ -1,7 +1,7 @@
 # Covid vaccination in Greece by department.
 # Data from gov.gr, 25 November 2021
 # Not necessarily representative because recorded by
-# vaccination department and by residence.
+# vaccination department and not by residence.
 # See it just as an exercice of combining python and R i a script
 import requests
 url = 'https://data.gov.gr/api/v1/query/mdg_emvolio?date_from=2021-11-25&date_to=2021-11-25'
